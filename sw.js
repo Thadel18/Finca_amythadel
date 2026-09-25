ec7173ebe6c1e97ad068b2888c53fe08204533e8
Thadel18 <klevtanguila@gmail.com>
2026-09-24 20:38:52 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,10 @@
-const CACHE_NAME = 'finca-amythadel-cache-v2';
-const ARCHIVOS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+const CACHE_NAME = 'finca-amythadel-cache-v3';
+const ARCHIVOS = [
+  './index.html', './manifest.json', './icon-192.png', './icon-512.png',
+  'https://www.gstatic.com/firebasejs/10.14.0/firebase-app-compat.js',
+  'https://www.gstatic.com/firebasejs/10.14.0/firebase-auth-compat.js',
+  'https://www.gstatic.com/firebasejs/10.14.0/firebase-firestore-compat.js'
+];
 
 self.addEventListener('install', (evento) => {
   evento.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ARCHIVOS)));
